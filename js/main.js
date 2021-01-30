930e62dbdd27afb5c22cb018631afdef4d1508cb
米哈依尔 <gm2308483@gmail.com>
2021-01-31 01:03:06 +0300

    add

--- js/main.js
+++ js/main.js
@@ -1,16 +1,37 @@
 
 
 
-function addApp(){
 
+	//сервис-воркер
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./service-worker.js')
-  .then(function(registration) {
+  navigator.serviceWorker.register('/pwa/service-worker.js').then(function(registration) {
     console.log('Registration successful, scope is:', registration.scope);
-  })
-  .catch(function(error) {
-    console.log('Service worker registration failed, error:', error);
-  });
-};
-}
-document.getElementById('add-app').onclick = addApp();
+  }).catch(function(error) {
+    console.log('Service worker registration failed, error:', error);});};
+
+
+
+let deferredPrompt;
+const addBtn = document.querySelector('.add-app');
+
+window.addEventListener('beforeinstallprompt', (e)) => {
+	//отключить автоприглашение
+	e.preventDefault();
+	deferredPrompt = e;
+	addBtn.style.display = 'block';
+
+	addBtn.addEventListener('click', () => {
+		addBtn.style.display = 'none';
+		// Show the prompt
+		deferredPrompt.prompt();
+// Wait for the user to respond to the prompt
+		deferredPrompt.useChoice.then((choiseResult) => {
+	if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the prompt');
+      } else {
+        console.log('User dismissed the prompt');
+      }
+      deferredPrompt = null;});
+		});
+	});
+
