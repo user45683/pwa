75743adf60ec1ee522372f0e79da2d8cf4ff53be
米哈依尔 <gm2308483@gmail.com>
2021-01-31 02:38:03 +0300

    add

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ if ('serviceWorker' in navigator) {
 // Code to handle install prompt on desktop
 let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
+addBtn.style.display = 'block';
 
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
