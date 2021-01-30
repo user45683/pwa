4419ca53a5fb3c7e1343060de29089a5f89fcdac
米哈依尔 <gm2308483@gmail.com>
2021-01-30 23:34:20 +0300

    add

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,23 @@
 
 
+const cacheName = "pwa";
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js'
+];
 
 // Listen for install event, set callback
 self.addEventListener('install', function(event) {
-    // Perform some task
+	event.waitUntil(caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
 
-self.addEventListener('activate', function(event) {
-  // Perform some task
+self.addEventListener('activate', function(event) {event.respondWith(caches.match(e.request).then(function(response) {return response || fetch(event.request);
+    })
+  );
 });
