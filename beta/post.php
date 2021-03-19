57c485b291e4b89249391e6b0fc8615e0526e66d
Your Name <you@example.com>
2021-03-20 02:19:49 +0300

    add

--- beta/post.php
+++ beta/post.php
@@ -1,15 +1,15 @@
 
-<meta http-equiv='refresh' content='3; url=http://localhost:8080/pwa/beta/index.html'>
+<meta http-equiv='refresh' content='3; url=https://dellsin.000webhostapp.com/'>
 <meta charset="utf-8">
 <?php
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$message = $_POST['message'];
-
-	$emailmessage = "Имя: $name /nE-mail: $email /nТекст: $message";
-	$email0 = "gm2308483@gmail.com";
-	$send = mail($email0, $emailmessage, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
-	if($send==true){
+$to = "gm2308483@gmail.com";
+$message = "Имя: " . $_POST['name'] . "<br>" ;
+$message .= "E-mail: " . $_POST['email'] . "<br>";
+$message .= $_POST['message'];
+$tema = "Обратная связь";
+$headers = "Content-type: text/plain; charset=\"utf-8\"\r\n";
+$mail = mail($to, $tema, $message, $headers);
+	if($mail==true){
 		echo "Сообщение отправлено";}
 	else{echo "Что-то пошло не так";}
 ?>
